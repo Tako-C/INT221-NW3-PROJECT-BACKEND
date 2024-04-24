90ff807aeb7e3532991e3a8883c65da040fef83b
Tako-c <phakphum2phrajrern@gmail.com>
2024-04-24 22:56:37 +0700

    fucntion convert time zone

--- src/main/java/sit/int221/mytasksservice/entities/MyTasks.java
+++ src/main/java/sit/int221/mytasksservice/entities/MyTasks.java
@@ -1,19 +1,28 @@
 package sit.int221.mytasksservice.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Data
 @Table(name = "mytasks")
 public class MyTasks {
-    @Id
-    private Integer id;
-    private String title;
-    private String description;
-    private String assignees;
-    private String status;
-    private String create_time;
-    private String update_time;
+
+        @Id
+        private Integer id;
+        private String title;
+        private String description;
+        private String assignees;
+        private String status;
+
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
+        private LocalDateTime create_Time;
+
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
+        private LocalDateTime update_Time;
+
 }
